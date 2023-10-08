0a8bc907fbcbd09532065559fc83d2ffdeeb028e
Muzamil Pirzada <74117614+muzamilpirzada@users.noreply.github.com>
2023-10-08 15:53:51 +0500

    complete expanding cards

---
+++ main.js
@@ -0,0 +1,15 @@
+const panels =document.querySelectorAll('.panel');
+
+panels.forEach(panel=>{
+    panel.addEventListener('click',function(){
+        removeActiveclasses();
+        panel.classList.add('active');
+    })
+});
+
+function removeActiveclasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
+    })
+}
+
